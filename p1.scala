7d97524c2950c5bf63afd026451d4cf01706e00c
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2016-01-21 07:52:11 -0300

    Create p1.scala

---
+++ p1.scala
@@ -0,0 +1,15 @@
+#!/bin/bash
+exec scala "$0" "$@"
+!#
+
+import scala.io.StdIn._
+import sys.process._
+
+println("Data e hora:")
+"date".!
+println
+println("Uso do disco:") 
+"df".!
+println
+println("Usuários conectados:")
+"w".!
